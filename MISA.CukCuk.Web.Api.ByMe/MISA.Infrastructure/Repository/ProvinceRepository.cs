cb3499b89e293deb138552fa5951287f67253ab3
Do Trong Hieu <hieu.dt173116@sis.hust.edu.vn>
2021-04-15 23:44:03 +0700

    filter

--- MISA.CukCuk.Web.Api.ByMe/MISA.Infrastructure/Repository/ProvinceRepository.cs
+++ MISA.CukCuk.Web.Api.ByMe/MISA.Infrastructure/Repository/ProvinceRepository.cs
@@ -1,7 +1,10 @@
-﻿using MISA.Core.Entities;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MISA.Core.Entities;
 using MISA.Core.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +13,16 @@ namespace MISA.Infrastructure.Repository
 {
    public  class ProvinceRepository:BaseRepository<Province>,IProvinceRepository
     {
+        public IActionResult GetByCountryId(Guid countryId)
+        {
+            var storeName = $"Proc_GetProvinceByCountryId";
+            var storeParam = new
+            {
+                CountryId = countryId
+            };
+
+            var entities = _dbConnection.Query<Province>(storeName, param: storeParam, commandType: CommandType.StoredProcedure);
+            return Ok(entities);
+        }
     }
 }
